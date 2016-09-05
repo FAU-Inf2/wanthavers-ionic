26c9c2ab7ee7822b1f3c6e5d4a7ae2cd67b8631d
philipp <philsippl@gmail.com>
2016-09-05 10:39:26 +0200

    testing if keyboard gets initialized

--- www/js/app.js
+++ www/js/app.js
@@ -18,6 +18,7 @@ wanthaver.run(['$ionicPlatform', 'PushNotifications', function($ionicPlatform, P
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
+        alert("keyboard init");
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
     }
